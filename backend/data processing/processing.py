bee02f0f3dcf6bc9251ee276e6ae24fe4b2bed61
Salma Azzam <salmaazzam@Salmas-MacBook-Pro.local>
2025-11-26 16:08:27 +0000

    blah

--- backend/data processing/processing.py
+++ backend/data processing/processing.py
@@ -4,4 +4,21 @@ df = pd.read_csv("backend/data/Copy of Housing_Pipeline_Long_List_External_Hacka
                 encoding="latin1",
                 usecols=["Project Name ", "Pipeline First Cut ", "Number Of Units ", "Planning  Status ", "One Line Summary "]
                 )
-print(df)
+
+
+# Pipeline First Cut filters - Short, Long, Medium, In Delivery 
+
+def filter(df, column, user_value):
+    return df[df[column].astype(str).str.lower().str.contains(user_value.lower())]
+
+
+f = filter(df, "Pipeline First Cut ", "In Delivery")
+
+
+print(f)
+
+# "Project Name " - , 
+# "Pipeline First Cut " - , 
+# "Number Of Units ", 
+# "Planning  Status ", 
+# "One Line Summary "
